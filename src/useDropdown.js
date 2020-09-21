a7d984831531eaeaabed861def6182ccfde19525
Franyeliza <franyelizarodriguez@live.com>
2020-09-21 12:40:46 -0400

    useEffect added

--- src/useDropdown.js
+++ src/useDropdown.js
@@ -12,7 +12,7 @@ const useDropdown = (label, defaultState, options) => {
         value={state}
         onChange={(e) => setState(e.target.value)}
         onBlur={(e) => setState(e.target.value)}
-        //disabled={options.length === 0}
+        disabled={options.length === 0}
       >
         <option>All</option>
         {options.map((item) => (
